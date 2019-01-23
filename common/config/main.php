c289521d797374850aca91334d1e56c1b28b667d
Hoybin <hoybin@outlook.com>
2019-01-23 17:13:44 +0800

    Add common/config/i18n

--- common/config/main.php
+++ common/config/main.php
@@ -5,12 +5,21 @@ return [
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'language' => 'zh-CN',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
         'authManager' => [
             'class' => 'yii\rbac\DbManager',
+        ],
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                ],
+            ],
         ],
     ],
 ];
